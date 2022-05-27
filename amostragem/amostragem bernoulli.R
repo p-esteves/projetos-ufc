7530012f001078a20d53aed460e0a3f2b7e77996
p-esteves <70980614+p-esteves@users.noreply.github.com>
2022-05-27 15:05:47 -0300

    -
    Adicionado algoritmo de Bernoulli e atualização de nome pro arquivo com Poisson, sistemática e PPT

---
+++ amostragem/amostragem bernoulli.R
@@ -0,0 +1,37 @@
+#script que realiza amostragem de bernoulli com parametros dados
+#semente aleatoria
+set.seed(404488)
+
+#parametros
+p = 0.5 #probabilidade de escolha
+N = 20 #tamanho da populacao
+
+#vetor onde os elementos escolhidos ficarao
+chosen = c()
+
+#numeros aleatorios
+e = runif(N,0,1)
+  
+#algoritmo
+#escolher individuos com ek < p
+for(i in 1:N) {
+    if(e[i]<p){
+      chosen <- c(chosen, i)
+      
+    }
+  }
+
+#amostra escolhida e seu tamanho
+n=length(chosen);paste0("foram escolhidos ",n," elementos: ");chosen
+
+
+
+
+
+
+
+
+
+
+
+
